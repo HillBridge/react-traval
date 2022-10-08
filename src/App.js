32b31ed67f89ebbab835f58a5cf58cd6918cbdc0
qiaoguosheng <294238921@qq.com>
2022-10-08 18:33:44 +0800

    产品分类组件设计与开发

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import styles from './App.module.css'
-import { Header, Footer, SideMenu, Carousel } from './components'
-import { Row, Col } from 'antd'
+import {
+  Header,
+  Footer,
+  SideMenu,
+  Carousel,
+  ProductCollection,
+} from './components'
+import { Row, Col, Typography } from 'antd'
+import { productList1, productList2, productList3 } from './mockups'
+import sideImage from './assets/images/sider_2019_02-04.png'
+import sideImage2 from './assets/images/sider_2019_02-04-2.png'
+import sideImage3 from './assets/images/sider_2019_12-09.png'
 
 function App() {
   return (
@@ -19,6 +29,33 @@ function App() {
             </div>
           </Col>
         </Row>
+        <ProductCollection
+          title={
+            <Typography.Text level={3} type="warning">
+              爆款推荐
+            </Typography.Text>
+          }
+          sideImage={sideImage}
+          products={productList1}
+        ></ProductCollection>
+        <ProductCollection
+          title={
+            <Typography.Text level={3} type="danger">
+              新品上市
+            </Typography.Text>
+          }
+          sideImage={sideImage2}
+          products={productList2}
+        ></ProductCollection>
+        <ProductCollection
+          title={
+            <Typography.Text level={3} type="secondary">
+              国内游推荐
+            </Typography.Text>
+          }
+          sideImage={sideImage3}
+          products={productList3}
+        ></ProductCollection>
       </div>
       <Footer></Footer>
     </div>
