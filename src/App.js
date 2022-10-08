3f89cf5232429b6b3bee7c6344fe76a6fbb85183
qiaoguosheng <294238921@qq.com>
2022-10-08 19:31:59 +0800

    react-router-dom v5版本处理

--- src/App.js
+++ src/App.js
@@ -1,65 +1,14 @@
 import styles from './App.module.css'
-import {
-  Header,
-  Footer,
-  SideMenu,
-  Carousel,
-  ProductCollection,
-  BusinessPartner
-} from './components'
-import { Row, Col, Typography } from 'antd'
-import { productList1, productList2, productList3 } from './mockups'
-import sideImage from './assets/images/sider_2019_02-04.png'
-import sideImage2 from './assets/images/sider_2019_02-04-2.png'
-import sideImage3 from './assets/images/sider_2019_12-09.png'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HomePage } from './pages';
+
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header></Header>
-      <div className={styles['page-content']}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <div style={{ background: 'red' }}>
-              <SideMenu></SideMenu>
-            </div>
-          </Col>
-          <Col span={18}>
-            <div style={{ background: 'blue' }}>
-              <Carousel></Carousel>
-            </div>
-          </Col>
-        </Row>
-        <ProductCollection
-          title={
-            <Typography.Text level={3} type="warning">
-              爆款推荐
-            </Typography.Text>
-          }
-          sideImage={sideImage}
-          products={productList1}
-        ></ProductCollection>
-        <ProductCollection
-          title={
-            <Typography.Text level={3} type="danger">
-              新品上市
-            </Typography.Text>
-          }
-          sideImage={sideImage2}
-          products={productList2}
-        ></ProductCollection>
-        <ProductCollection
-          title={
-            <Typography.Text level={3} type="secondary">
-              国内游推荐
-            </Typography.Text>
-          }
-          sideImage={sideImage3}
-          products={productList3}
-        ></ProductCollection>
-        <BusinessPartner></BusinessPartner>
-      </div>
-      <Footer></Footer>
+      <Router>
+        <Route path="/" component={HomePage} exact></Route>
+      </Router>
     </div>
   )
 }
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Header,
+  Footer,
+  SideMenu,
+  Carousel,
+  ProductCollection,
+  BusinessPartner,
+} from '../../components'
+import { Row, Col, Typography } from 'antd'
+import { productList1, productList2, productList3 } from './mockups'
+import sideImage from '../../assets/images/sider_2019_02-04.png'
+import sideImage2 from '../../assets/images/sider_2019_02-04-2.png'
+import sideImage3 from '../../assets/images/sider_2019_12-09.png'
+import styles from "./HomePage.module.css";
+
+export class HomePage extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <>
+        <Header></Header>
+        <div className={styles['page-content']}>
+          <Row style={{ marginTop: 20 }}>
+            <Col span={6}>
+              <div style={{ background: 'red' }}>
+                <SideMenu></SideMenu>
+              </div>
+            </Col>
+            <Col span={18}>
+              <div style={{ background: 'blue' }}>
+                <Carousel></Carousel>
+              </div>
+            </Col>
+          </Row>
+          <ProductCollection
+            title={<Typography.Text type="warning">爆款推荐</Typography.Text>}
+            sideImage={sideImage}
+            products={productList1}
+          ></ProductCollection>
+          <ProductCollection
+            title={<Typography.Text type="danger">新品上市</Typography.Text>}
+            sideImage={sideImage2}
+            products={productList2}
+          ></ProductCollection>
+          <ProductCollection
+            title={
+              <Typography.Text type="secondary">国内游推荐</Typography.Text>
+            }
+            sideImage={sideImage3}
+            products={productList3}
+          ></ProductCollection>
+          <BusinessPartner></BusinessPartner>
+        </div>
+        <Footer></Footer>
+      </>
+    )
+  }
+}
+
